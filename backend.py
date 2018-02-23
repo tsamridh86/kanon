28c959b7354c1c94b34a7d575bca132be99cfac6
tsamridh86 <t.samridh.86@gmail.com>
2018-02-24 00:15:56 +0530

    eased navigation

---
+++ backend.py
@@ -0,0 +1,334 @@
+import threading
+import networkx as nx
+import matplotlib.pyplot as plt
+from random import randrange
+
+class fileReaderThread (threading.Thread):
+   def __init__(self, threadID, name , function, fileName):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
+      self.function = function
+      self.fileName = fileName
+   def run(self):
+      # print ("Starting " + self.name)
+      # self.function(self.fileName)
+      # print ("Exiting " + self.name)
+      pass
+
+# nodes = []
+# edges = []
+
+def nodeReader(fileName , nodes):
+   try:
+      with open(fileName,"r") as file:
+         for line in file:
+            try:
+               nodes.append(int(line))
+            except ValueError as v:
+               print ("file found an invalid node : ", v)
+               continue
+      return nodes
+   except FileNotFoundError as e :
+      print("file was not found : ", e)
+
+def edgeReader(fileName,edges):
+   try:
+      with open(fileName, "r") as file:
+         for line in file:
+            try:
+               v1,v2 = line.split(' ')
+               edges.append((int(v1),int(v2)))
+            except ValueError as v:
+               print("file has an invalid edge : ",v)
+               continue
+      return edges
+   except FileNotFoundError as e :
+      print("file was not found : ", e)
+
+def drawGraph(graph):
+   nx.draw(graph,node_size=100)
+   plt.savefig("plot.png")
+   plt.gcf().clear()
+
+def generateNetwork(nodes,edges,graph):
+   graph.add_nodes_from(nodes)
+   graph.add_edges_from(edges)
+   drawGraph(graph)
+   return graph
+
+def clearNetwork(graph):
+   graph.clear()
+   drawGraph(graph)
+   return graph
+
+def kAnonimizer ( degreeSequence , nodeId , n , k):
+    originalDegreeSequence = degreeSequence
+    anonymizedDegreeSequence = degreeSequence
+    k_constant = k
+    for i in range(n):
+        if i < k :
+            if i == 0:
+                anonymizedDegreeSequence[i] = originalDegreeSequence[i]
+            else:
+                anonymizedDegreeSequence[i] = anonymizedDegreeSequence[i-1]
+        else:
+            if ( ( k + k_constant) < n ):
+                d_ik = anonymizedDegreeSequence[i - k_constant]
+                d_i = anonymizedDegreeSequence[i]
+                idk1 = 0 
+                idk2 = 0 
+                for _len in range(k+2,k+k_constant):
+                    idk1 = idk1 + anonymizedDegreeSequence[k+2] - anonymizedDegreeSequence[_len]
+                c_merge = d_ik - d_i + idk1
+                for _len in range(k+1,k+k_constant):
+                    idk2 = idk2 + anonymizedDegreeSequence[k+1] - anonymizedDegreeSequence[_len]
+                c_new = idk2
+                if c_new >= c_merge:
+                    anonymizedDegreeSequence[i] = anonymizedDegreeSequence[i-1]
+                    k += 1
+                else:
+                    k = i + k_constant + 1
+            else:
+                if ( k+k_constant == n ):
+                    k = n
+                else:
+                    k = n
+                    i-=1
+    return anonymizedDegreeSequence
+
+def removeValues( nodes ):
+    keylist = list(key for key in nodes)
+    for i in range(len(keylist)):
+        if nodes[keylist[i]] == 0:
+            nodes.pop(keylist[i])
+    return nodes
+
+def anonimize(graph, kValue):
+   view = nx.degree(graph)
+   view = sorted(view, key= lambda x: x[1] , reverse= True)
+   degreeSequence = list(value for key,value in view)
+   nodeId = list(key for key,value in view)
+   initialView = {}
+   print("degSeq initial",degreeSequence)
+   for key,value in view:
+       initialView[key] = value
+   degreeSequence = (kAnonimizer(degreeSequence,nodeId,len(nodeId),kValue))
+
+   print("degSeq from kanon",degreeSequence)
+   extra = len(nodeId) % kValue
+   print("extra",extra)
+   if extra != 0:
+       ne = degreeSequence[len(nodeId)-extra-1]
+       for i in range(len(nodeId)-1,len(nodeId)-extra-1,-1):
+           degreeSequence[i] = ne
+           print(ne,degreeSequence)  
+
+   finalView = {}
+   for i in range(len(nodeId)):
+       finalView[nodeId[i]] = degreeSequence[i]
+   remaining = {}
+   for key in initialView:
+       if initialView[key] != finalView[key]:
+           remaining[key] = finalView[key] - initialView[key]
+   print("bfor while remaining",remaining)
+   z = 1          
+   # node connector
+   
+   while len(remaining) > 1:
+      if z > len(remaining):
+        break
+      i=0  
+      while i < len(remaining): 
+      #for i in range(len(remaining)):
+      
+         remaining = removeValues(remaining)
+         keylist = list(key for key in remaining)
+         print("remaining key list",keylist)
+         j = i + 1
+         print("i -j-",i,j)
+         x = len(keylist)
+         print("x--",x)
+         while j < x:
+      
+            if (keylist[i], keylist[j]) not in graph.edges():
+               graph.add_edge(keylist[i],keylist[j])
+               remaining[keylist[i]]-=1
+               remaining[keylist[j]]-=1
+               remaining = removeValues(remaining)
+               keylist = list(key for key in remaining)
+               print("remaining in if ",remaining)
+               break
+            else:
+               print("in else") 
+               randomIndex = randrange(0,len(keylist))
+               if((keylist[i],keylist[randomIndex]) not in graph.edges() and i != randomIndex):     
+                   print("randomIndex",randomIndex)
+                   print("in else connected with", keylist[randomIndex])
+                   graph.add_edge(keylist[i],keylist[randomIndex])
+                   remaining[keylist[i]]-=1
+                   remaining[keylist[randomIndex]]-=1 
+                   remaining = removeValues(remaining)
+                   keylist = list(key for key in remaining)
+                   break  
+            print("in while remaining", remaining)
+            print("keylist",keylist)
+            x = len(keylist)
+            j+=1
+
+         print("after while i ",i)
+         i+=1
+
+      z+=1
+            
+         
+   remaining = removeValues(remaining)
+   keylist = list(key for key in remaining)
+   keyseq = readyToConnect(graph, kValue)
+   s=0
+   while(len(remaining)>0):
+      if(s>len(remaining)):
+        print("there is still remaining nodes")
+        break
+      # if (checkAlldegree(graph,kValue)):
+      #   print("all satisfied")
+      #   break
+      i=0
+      if(keylist[i] in remaining and len(keylist)>=i):
+        print("key[i] is in remainning-",keylist[i])
+        print("i",i)
+        print("keyseq",keyseq)
+        print("remain",remaining)
+        if len(keyseq)==0:
+          print("there no node avail to connect with remainings")
+          break
+        for p in keyseq:
+
+          if((keylist[i], p) not in graph.edges() and keylist[i] != p):
+            graph.add_edge(keylist[i], p)
+            remaining[keylist[i]]-=1
+            keyseq.remove(p)
+            remaining = removeValues(remaining)
+            keylist = list(key for key in remaining)
+            print("connected  with",p)
+            break
+ 
+      #s+=1
+   oo = readyToConnect(graph,kValue)  #to print degree,val deictionary
+   drawGraph(graph)
+   return graph
+def checkAlldegree(graph, kValue):
+  oldDict = nx.degree(graph)
+  newDict = {}
+  for key,value in oldDict:
+    if value not in newDict:
+      newDict[value] = 1
+    else:
+      newDict[value]+=1
+  for key, value in newDict.items():
+     if value < kValue:
+        return False    
+  return True
+def readyToConnect(graph, kValue):
+  oldDict = nx.degree(graph)
+  newDict = {}
+  remain2 = []
+  nseq = []
+  keyseq = []
+  for key,value in oldDict:
+    if value not in newDict:
+      newDict[value] = 1
+    else:
+      newDict[value]+=1
+  print("graph node,degree",oldDict)     
+  print("new dect-deg,sum",newDict)
+
+                 # #ki is deg
+                 # #vi is total nodes having ki same deg 
+  for key, value in newDict.items():                  
+    if value > kValue :
+      for z in range(1,value-kValue+1):
+         nseq.append(key)
+    else:
+      for m in range(1,kValue-value+1):
+         remain2.append(key)
+  #newDict[value+1] >= kValue-1 
+  for key,value in oldDict:
+     if value in nseq:
+        keyseq.append(key)
+  print("nseq",nseq)    
+  print("keyseq",keyseq)
+  return keyseq
+
+def nodeDegreeLessThanK(k, val, graph):
+  dict1 = nx.degree(graph)
+
+
+def getstats(graph):
+  mainstring = "Stats:\n"
+  mainstring += "No of Nodes : "+str(len(graph))+"\n"
+  mainstring += "No of Edges : "+str(graph.number_of_edges())+"\n"
+  degree_list=[]
+  for key,value in nx.degree(graph):
+  	degree_list.append(value) 
+  try:
+  	avgDegree = sum(degree_list)/len(degree_list)
+  	avgPathDegree = 0
+  	avgClustering = 0
+  	edgeBetweeness = 0
+  	temp = {}
+  	if nx.is_connected(graph):
+  		avgPathDegree = nx.average_shortest_path_length(graph)
+  		avgClustering = nx.average_clustering(graph)
+  		temp = nx.edge_betweenness_centrality(graph)
+  	mainstring += "Avg Degree : {0:.4f} \n"
+  	mainstring += "Avg path len: {1:.4f} \n"
+  	mainstring += "Avg Clustering : {2:.4f} \n"
+  	mainstring += "Avg Betweeness : {3:.4f} \n"
+  	if len(temp) > 0:
+  		for key, value in temp:
+  			edgeBetweeness += value
+  		edgeBetweeness /= len(temp)
+  	return mainstring , avgDegree , avgPathDegree , avgClustering, edgeBetweeness
+  except ZeroDivisionError:
+  	mainstring += "Avg Degree : - \n"
+  	mainstring += "Avg Path len: - \n"
+  	mainstring += "Avg Clustering: - \n"
+  	mainstring += "Betweeness Centraility : {3:.4f} \n"
+  	return mainstring , 0 , 0 , 0 , 0
+  except Exception as e:
+  	print(e)
+  	return mainstring
+
+def saveToFile(directory,graph):
+   with open(directory+"/nodeList.txt","w") as nodeFile:
+      for node in graph.nodes():
+         nodeFile.write(str(node)+"\n")
+   with open(directory+"/edgeList.txt","w") as edgeFile:
+      for v1,v2 in graph.edges():
+         edgeFile.write(str(v1)+" "+str(v2)+"\n")
+   with open(directory+"/clustering.txt","w") as clusterFile:
+   		clusterFile.write(str(nx.clustering(graph)))
+   with open(directory+"/betweenness.txt","w") as betweenFile:
+   		betweenFile.write(str(nx.edge_betweenness_centrality(graph)))
+   with open(directory+"/degrees.txt","w") as degreeFile:
+   		degreeFile.write(str(nx.degree(graph)))
+
+# print(nodes, edges)
+# Create new threads
+
+# thread1 = fileReaderThread(1, "nodeInputThread", nodeReader, "nodeList.txt")
+# thread2 = fileReaderThread(2, "edgeInputThread", edgeReader, "edgeList.txt")
+
+# Start new Threads
+# thread1.start()
+# thread2.start()
+# thread1.join()
+# thread2.join()
+
+
+# G.add_nodes_from(nodes)
+# G.add_edges_from(edges)
+# nx.draw(G)
+# plt.savefig("plot.png")
+# plt.show()
---
+++ gui.py
@@ -0,0 +1,90 @@
+import sys
+from PyQt4 import QtCore, QtGui,uic
+from backend import *
+
+class MyWindow(QtGui.QMainWindow):
+
+    nodes = []
+    edges = []
+    graph = nx.Graph()
+
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        uic.loadUi('gui.ui', self)
+        self.show()
+        self.applyButton.clicked.connect(self.applyButtonClicked)
+        self.kValueSlider.valueChanged.connect(self.displayChange)
+        self.kValueHolder.setText(str(self.kValueSlider.value()))
+        self.actionAdd_Node.triggered.connect(self.nodeOpener)
+        self.actionAdd_Edge.triggered.connect(self.edgeOpener)
+        self.actionExit.triggered.connect(self.goOut)
+        self.actionRemove_Files.triggered.connect(self.clearFiles)
+        self.actionSave_as.triggered.connect(self.saveFile)
+        self.graph = self.plotNetwork()
+
+    def applyButtonClicked(self):
+        print("Apply button pressed ",self.kValueSlider.value())
+        kValue = int(self.kValueHolder.text())
+        if kValue > len(self.graph):
+            kValue = len(self.graph)-1
+            print("Exceeded max k value")
+        self.kValueSlider.setValue(kValue)
+        anonimize(self.graph,kValue)
+        self.plotNetwork()
+
+    def displayChange(self, val):
+        self.kValueHolder.setText(str(val))
+
+    def nodeOpener(self):
+        nodeFile = self.fileOpener()
+        self.nodes = nodeReader(nodeFile,self.nodes)
+        self.graph = self.plotNetwork()
+        self.setMaxSliderValue()
+
+    def edgeOpener(self):
+        edgeFile = self.fileOpener()
+        self.edges = edgeReader(edgeFile,self.edges)
+        self.graph = self.plotNetwork()
+        self.setMaxSliderValue()
+
+    def fileOpener(self):
+        dlg = QtGui.QFileDialog()
+        dlg.setFileMode(QtGui.QFileDialog.AnyFile)
+        dlg.setFilter("Text files (*.txt)")
+        dlg.exec_()
+        fileNames = dlg.selectedFiles()
+        return (fileNames[0])
+
+    def plotNetwork(self):
+        graph = generateNetwork(self.nodes,self.edges,self.graph)
+        self.imageHolder.setPixmap(QtGui.QPixmap('plot.png'))
+        statstring, avgDegree , avgPathLenght , avgClustering , edgeBetweeness = getstats(graph)
+        self.statsHolder.setPlainText(statstring.format(avgDegree,avgPathLenght,avgClustering,edgeBetweeness))
+        return graph
+
+    def clearFiles(self):
+        self.nodes = []
+        self.edges = []
+        self.graph = clearNetwork(self.graph)
+        self.graph = self.plotNetwork()
+
+    def goOut(self):
+        sys.exit()
+
+    def setMaxSliderValue(self):
+        if len(self.graph) == 0:
+            self.kValueSlider.setMaximum(16)
+        else:
+            self.kValueSlider.setMaximum(len(self.graph))
+
+    def saveFile(self):
+        dlg = QtGui.QFileDialog()
+        dlg.setFileMode(QtGui.QFileDialog.DirectoryOnly)
+        dlg.exec_()
+        folderName = dlg.selectedFiles()
+        saveToFile(folderName[0],self.graph)
+        
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    window = MyWindow()
+    sys.exit(app.exec_())
